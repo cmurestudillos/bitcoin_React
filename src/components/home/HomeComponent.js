eec656757b3d1ead5ccf8db1069a6069bab9769d
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2020-11-20 08:33:48 +0100

    Sistema de Navegación: Pestañas

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+
+import {Paper, Tabs, Tab, Card} from '@material-ui/core/';
 // Peticiones Http
 //import axios from 'axios';
 // EndPoint
 //import url from '../../api/endpoint';
 
 class HomeComponent extends Component{
+    state = {
+        value: 0
+    }
     //----------------------------------------------------------------------//
     // Antes de renderizar, cargar el metodo                                //
     //----------------------------------------------------------------------//
@@ -12,6 +17,11 @@ class HomeComponent extends Component{
         // Log de seguimiento
         console.log("HomeComponent.js - Metodo componentWillMount");  
     }
+    handleChange = (event, newValue) => {
+        this.setState({
+            value: newValue
+        })
+      };
     //----------------------------------------------------------------------//
     // Metodo render                                                        //
     //----------------------------------------------------------------------//
@@ -22,6 +32,20 @@ class HomeComponent extends Component{
         return(
             <div>
                 <h1>Home component</h1>
+                    <Tabs indicatorColor="primary" textColor="primary" centered value={this.state.value} onChange={this.handleChange}>
+                        <Tab label="Item One" value={0} index={0}/> Hola 
+                        <Tab label="Item Two" value={1} index={1}/> Adios
+                        <Tab label="Item Three" value={2} index={1} /> bienvenido
+                    </Tabs>
+                <Card value={this.state.value} index={0}>
+                    Item One
+                </Card>    
+                <Card value={this.state.value} index={1}>
+                    Item Dos
+                </Card> 
+                <Card value={this.state.value} index={2}>
+                    Item Res
+                </Card>                                 
             </div>
         );
     }
