288589db4570b08b33cdd0d828fe45a78cb97436
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2020-11-25 08:38:04 +0100

    Pestaña Euro - I

---
+++ src/components/euro/EuroComponent.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+// Peticiones Http
+import axios from 'axios';
+// EndPoint
+import dataEur from '../../api/eur.js';
+// Componentes Material Ui
+import { Card, CardContent, Avatar, Chip, Grid} from '@material-ui/core/';
+//import dataUsd from '../../api/usd.js';
+//import dataGbp from '../../api/gbp.js';
+// Imagenes
+import euro from '../../assets/img/euro.png';
+class EuroComponent extends Component{
+
+    state = {
+        monedaEur: []
+    }
+
+    //----------------------------------------------------------------------//
+    // Antes de renderizar, cargar el metodo                                //
+    //----------------------------------------------------------------------//
+    componentDidMount (){  
+        // Log de seguimiento
+        console.log("EuroComponent.js - Metodo componentWillMount");  
+
+        // Obtenemos los datos de Euro
+        this.getEurCurerency();
+    }
+//-------------------------------------------------------//
+// Metodo para obtener datos en Euros                    //
+//-------------------------------------------------------//
+getEurCurerency(oldvalue, newvalue){
+
+    // Log de seguimiento
+    console.log('EuroComponent.js - Metodo getEurCurerency');
+
+    axios.get(dataEur.endpoint)
+        .then( res => {
+            if(res.data){
+                this.setState({
+                    monedaEur: res.data['bpi'].EUR
+                })
+        }
+    });
+
+}    
+    //----------------------------------------------------------------------//
+    // Metodo render                                                        //
+    //----------------------------------------------------------------------//
+    render(){
+        // Log de seguimiento
+        console.log('EuroComponent.js - Metodo render()');
+
+        return(
+            <div>
+                <Card className="card">
+                    <CardContent>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <img src={euro} width="150" height="150" className="imagen" alt={this.state.monedaEur.description} title={this.state.monedaEur.description} />
+                            <Chip avatar={<Avatar>€</Avatar>} label={this.state.monedaEur.rate}  className="badge" clickable color="primary" variant="outlined" /> 
+                        </Grid>
+                    </Grid>
+
+                    </CardContent>
+                </Card>                            
+            </div>
+        );
+    }
+}
+
+export default EuroComponent;
--- src/components/tabpanel/TabPanelComponent.js
+++ src/components/tabpanel/TabPanelComponent.js
@@ -1,32 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // Componentes Material Ui
-import {AppBar, Tabs, Tab, Typography, Box, Card, CardContent} from '@material-ui/core/';
-// Peticiones Http
-import axios from 'axios';
-// EndPoint
-import dataEur from '../../api/eur.js';
-//import dataUsd from '../../api/usd.js';
-//import dataGbp from '../../api/gbp.js';
-// Imagenes
-import euro from '../../assets/img/euro.png';
-
-//-------------------------------------------------------//
-// Metodo para obtener datos en Euros                    //
-//-------------------------------------------------------//
-function getEurCurerency(oldvalue, newvalue){
-
-    // Log de seguimiento
-    console.log('TabPanelComponents.js - Metodo getEurCurerency');
-
-    axios.get(dataEur.endpoint)
-        .then( res => {
-            if(res.data){
-              newvalue = res.data['bpi'].EUR;
-              console.log(newvalue);
-        }
-    });
-}
+import {AppBar, Tabs, Tab, Typography, Box} from '@material-ui/core/';
+// Componentes
+import EuroComponent from '../euro/EuroComponent';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -58,9 +35,6 @@ function a11yProps(index) {
 
 export default function TabPanelComponent() {
 
-  var oldDatosEuro = [];
-  var newDatosEuro = [];
-
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
@@ -71,19 +45,13 @@ export default function TabPanelComponent() {
     <div className="tabpanel">
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered>
-          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Euro" {...a11yProps(0)} />
           <Tab label="Item Two" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <Card className="root">
-        <CardContent>
-            <img src={euro} width="150" height="150" alt="Euro" />
-            {getEurCurerency(oldDatosEuro, newDatosEuro)}
-            {console.log(newDatosEuro)}
-        </CardContent>
-        </Card>
+        <EuroComponent />
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
